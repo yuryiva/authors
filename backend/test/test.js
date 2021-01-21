86c89cdf93f505f38b76b68beeab9c7ebe090212
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-01-21 14:05:55 +0300

    restructured BE

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-router.get("/test", (req, res) => {
+router.get("/", (req, res) => {
   console.log("hello TEST");
   res.send("hello TEST!");
 });
